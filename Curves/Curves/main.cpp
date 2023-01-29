4bfd6f05b424caf7187f576d70c54d4204de31b1
Mircurio <misha.152440@yandex.ru>
2023-01-29 16:47:59 +0300

    Developing

--- Curves/Curves/main.cpp
+++ Curves/Curves/main.cpp
@@ -1,46 +1,148 @@
 #include <iostream>
+#include <math.h>
+#include <vector>
+
+const double PI = 3.141592653589793;
+
 
 struct Point
 {
-	double x, y, z;
+	double x;
+	double y;
+	double z;
 };
 
 class Circle
 {
+private:
+
 	double radius;
+
+	double getX(double t)
+	{
+		return radius * cos(t);
+	}
+
+	double getY(double t)
+	{
+		return radius * sin(t);
+	}
+
 public:
 
-	Circle(double radius) : radius{ radius } {};
-
-	Point& getPoint(double t)
+	Circle(double radius)
 	{
+		if (radius <= 0)
+		{
+			throw "Radius cannot be negative or zero";
+		}
+		else
+		{
+			this->radius = radius;
+		}
+	}
 
+	// Возвращаем точку.
+	virtual Point& getPoint(double t)
+	{
+		return *(new Point{ getX(t), getY(t), 0 });
+	}
+
+	virtual double getRadius()
+	{
+		return radius;
+	}
+
+	virtual double getFirstDerivative(double t)
+	{
+		return cos(t) / sin(t);
 	}
 };
 
-class Ellipse
+class Ellipse: public Circle
 {
-	double radius1;
+private:
+
 	double radius2;
+
+	double getX(double t)
+	{
+		return Circle::getRadius() * cos(t);
+	}
+
+	double getY(double t)
+	{
+		return radius2 * sin(t);
+	}
+
 public:
 
-	Ellipse(double radius1, double radius2) : radius1{ radius1 }, radius2{ radius2 }{};
-
-	Point& getPoint(double t)
+	Ellipse(double radius1, double radius2) : Circle{ radius1 }
 	{
+		if (radius2 <= 0)
+		{
+			throw "Radius cannot be negative or zero";
+		}
+		else
+		{
+			this->radius2 = radius2;
+		}
+	}
 
+	// Возвращаем точку.
+	virtual Point& getPoint(double t)
+	{
+		return *(new Point{ getX(t), getY(t), 0 });
+	}
+
+	virtual double getFirstDerivative(double t)
+	{
+		return  (-radius2 / Circle::getRadius()) * (cos(t) / sin(t));
 	}
 };
 
-class Helix
+class Helix: public Circle
 {
-	double radius;
+private:
+
 	double step;
+
+	double getX(double t)
+	{
+		return Circle::getRadius() * cos(t);
+	}
+
+	double getY(double t)
+	{
+		return Circle::getRadius() * sin(t);
+	}
+
+	double getZ(double t)
+	{
+		return t / (2 * PI) * step;
+	}
+
 public:
 
-	Helix(double radius, double step) : radius{ radius }, step{ step }{};
+	Helix(double radius, double step) : Circle{ radius }
+	{
+		if (step < 0)
+		{
+			throw "Step cannot be negative";
+		}
+		else
+		{
+			this->step = step;
+		}
+	}
 
-	Point& getPoint(double t)
+	// Возвращаем точку.
+	virtual Point& getPoint(double t)
+	{
+		return *(new Point{ getX(t), getY(t), getZ(t) });
+	}
+
+	virtual double getFirstDerivative(double t)
 	{
 
 	}
